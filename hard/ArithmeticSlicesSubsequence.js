b9d288275533eb96544d27ac66d3fa905b453914
the-wind-dev <80009607+the-wind-dev@users.noreply.github.com>
2025-02-09 22:42:48 +0300

    Arithmetic Slices Subsequence

---
+++ hard/ArithmeticSlicesSubsequence.js
@@ -0,0 +1,48 @@
+/*
+Дан целочисленный массив nums, вернуть количество всех арифметических подпоследовательностей nums.
+Арифметическаая последовательность состоит минимум из 3х элементов.
+*/
+
+/*
+Input: nums = [2,4,6,8,10]
+Output: 7
+*/
+const NUMS = [2,4];
+
+class Arithmetic {
+    constructor() {
+        this.len = 0;
+        this.result = 0;
+    }
+
+    dfs(depth, numsArray, currentSub) {
+        console.log("len:",this.len);
+        console.log("depth:", depth);
+        console.log("numsArray:",numsArray);
+        console.log("currentSub:",currentSub);
+        if (depth === this.len) {
+            if (currentSub.length < 3) return;
+            for (let i = 1; i < currentSub.length; i++) {
+                if (currentSub[i] - currentSub[i - 1] !== currentSub[1] - currentSub[0]) return;
+            }
+            this.result++;
+            return;
+        }
+        this.dfs(depth + 1, numsArray, currentSub);
+        currentSub.push(numsArray[depth]);
+        this.dfs(depth + 1, numsArray, currentSub);
+        currentSub.pop();
+    }
+
+    numberOfArithmeticSlices(numsArray) {
+        this.len = numsArray.length;
+        this.result = 0;
+        this.dfs(0, numsArray, []);
+        return this.result;
+    }
+}
+
+const arithmetic = new Arithmetic();
+
+arithmetic.numberOfArithmeticSlices(NUMS);
+console.log(arithmetic.result);
